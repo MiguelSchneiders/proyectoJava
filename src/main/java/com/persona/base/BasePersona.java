76c6e45499c940fe6e3b3cab10af3d5991a00595
Miguel Sepulveda Alvarado <msepulveda@MacBook-Pro-de-Miguel.local>
2017-09-11 14:50:57 -0300

    todo

---
+++ src/main/java/com/persona/base/BasePersona.java
@@ -0,0 +1,12 @@
+package com.persona.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.persona.entity.PersonaEntity;
+
+public class BasePersona {
+
+	public static List<PersonaEntity> listaPersonas = new ArrayList<PersonaEntity>();
+
+}
